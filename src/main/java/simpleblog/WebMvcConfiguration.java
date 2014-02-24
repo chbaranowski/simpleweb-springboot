4d8d5c836c459069635d3eebf7965aeff19cab30
Christian Baranowski <Christian.Baranowski@seitenbau.com>
2014-02-24 09:23:27 +0100

    cleanup

--- src/main/java/simpleblog/WebMvcConfiguration.java
+++ src/main/java/simpleblog/WebMvcConfiguration.java
@@ -1,14 +1,8 @@
 package simpleblog;
 
-import javax.servlet.Filter;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
